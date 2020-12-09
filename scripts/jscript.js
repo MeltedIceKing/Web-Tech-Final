99ce880e5b53a50a30c1290075bc2ed573381735
Sahil Singh <shilsingh01@yahoo.com>
2020-12-09 14:25:20 -0800

    made it so that hovered image appears

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -1,9 +1,24 @@
 document.querySelector('.smallGrid').style.visibility = 'hidden';
+var imageDiv = document.querySelector('#thumb-bar')
+var listImages = [];
 
 let loopImages = () => {
-    var innerArray = document.querySelector('.images');
+    var innerArray = document.querySelectorAll('.images');
     console.log(innerArray);
-    // var imageArray = innerArray.split('\n')
+    for (var i = 0; i < innerArray.length; i++) {
+        lastElement = innerArray[i].src.split('/')
+        listImages.push(
+            lastElement[lastElement.length - 1]
+        );
+    };
+    console.log(listImages)
 };
 
 loopImages();
+
+
+imageDiv.addEventListener('mouseover', (e) => {
+    var itemToBeUsed = e.target.src;
+    document.querySelector('.displayed-img').src = itemToBeUsed
+    document.querySelector('.smallGrid').style.visibility = 'visible';
+});
