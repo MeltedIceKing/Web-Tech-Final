1a54f4961e0866cad1bf898e1772c6621dc8e7cb
Sahil Singh <shilsingh01@yahoo.com>
2020-12-09 13:50:35 -0800

    added some js

---
+++ scripts/jscript.js
@@ -0,0 +1,9 @@
+document.querySelector('.smallGrid').style.visibility = 'hidden';
+
+let loopImages = () => {
+    var innerArray = document.querySelector('.images');
+    console.log(innerArray);
+    // var imageArray = innerArray.split('\n')
+};
+
+loopImages();
