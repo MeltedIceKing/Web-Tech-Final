dc48378a8526ee7dd08db3bac648afea3bc2b6b6
Sahil Singh <shilsingh01@yahoo.com>
2020-12-09 14:44:11 -0800

    made it so that hovered image appears

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -1,24 +1,46 @@
 document.querySelector('.smallGrid').style.visibility = 'hidden';
-var imageDiv = document.querySelector('#thumb-bar')
+var inputButton = document.querySelector('button');
+var imageDiv = document.querySelector('#thumb-bar');
 var listImages = [];
 
 let loopImages = () => {
     var innerArray = document.querySelectorAll('.images');
-    console.log(innerArray);
     for (var i = 0; i < innerArray.length; i++) {
-        lastElement = innerArray[i].src.split('/')
+        lastElement = innerArray[i].src;
         listImages.push(
-            lastElement[lastElement.length - 1]
+            lastElement
         );
     };
-    console.log(listImages)
+};
+
+let clickedButton = () => {
+    var userInput = document.querySelector('#effect').value;
+    if (userInput == 'blur'){
+        var imageUsed = document.querySelector('.displayed-img').src;
+        for (var j = 0; j < listImages.length; j++){
+            if (listImages[0] == imageUsed){
+                document.querySelector('.displayed-img').src = 'images/pic1B.jpg';
+            }else if (listImages[1] == imageUsed){
+                document.querySelector('.displayed-img').src = 'images/pic2B.jpg';
+            }else if (listImages[2] == imageUsed){
+                document.querySelector('.displayed-img').src = 'images/pic3B.jpg';
+            }else if (listImages[3] == imageUsed){
+                document.querySelector('.displayed-img').src = 'images/pic4B.jpg';
+            }else if (listImages[4] == imageUsed){
+                document.querySelector('.displayed-img').src = 'images/pic5B.jpg';
+            }
+        };
+    } else {
+        alert('That is an invalid effect - try: blur')
+    }
 };
 
 loopImages();
 
-
 imageDiv.addEventListener('mouseover', (e) => {
     var itemToBeUsed = e.target.src;
-    document.querySelector('.displayed-img').src = itemToBeUsed
+    document.querySelector('.displayed-img').src = itemToBeUsed;
     document.querySelector('.smallGrid').style.visibility = 'visible';
 });
+
+inputButton.addEventListener('click', clickedButton)
